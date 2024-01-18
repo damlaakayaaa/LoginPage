03b3e1b476ead309a79ff9af953f298bb1c56d84
Damla Kaya <99400777+damlaakayaaa@users.noreply.github.com>
2024-01-18 09:41:29 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using STB.MAP.UI.Models;
+using STB.MAP.UI.Utility;
+using System;
+using Newtonsoft.Json;
+
+
+namespace STB.MAP.UI
+{
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
+
+			services.Configure<IdentityOptions>(options =>
+			{
+				options.Lockout.AllowedForNewUsers = false;
+				options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+				options.Lockout.MaxFailedAccessAttempts = 5;
+			});
+
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
+
+			services.AddHttpsRedirection(options =>
+			{
+				options.HttpsPort = 443;
+				options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+			});
+
+			services.AddControllers()
+				.AddNewtonsoftJson(options =>
+			{
+				options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+			});
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
+
+
+
+
+            services.AddSession();
+
+			services.AddScoped<IProjeRepository, ProjeRepository>();
+
+			services.AddRazorPages();
+
+			services.AddAuthorization(options =>
+			{
+				options.AddPolicy("LoggedInUsers", policy =>
+					policy.RequireAuthenticatedUser());
+
+				options.AddPolicy("LogOutPolicy", policy =>
+				{
+					policy.RequireClaim("LoggedIn", "true");
+				});
+			});
+
+			services.AddDistributedMemoryCache();
+
+			services.AddSingleton<ITempDataDictionaryFactory, TempDataDictionaryFactory>();
+
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+			})
+			.AddCookie(options =>
+			{
+				options.LoginPath = "/Account/Login";
+				options.AccessDeniedPath = "/Account/AccessDenied";
+				options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+				options.SlidingExpiration = true;
+
+				options.Cookie.Name = CookieAuthenticationDefaults.AuthenticationScheme;
+			});
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
+
+            app.UseCors("AllowAllOrigins");
+
+            app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
+			app.UseSession();
+
+			app.UseRouting();
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "areas",
+					pattern: "{area:exists}/{controller=App}/{action=Index}/{id?}");
+
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Account}/{action=Login}/{id?}");
+
+				endpoints.MapRazorPages();
+			});
+		}
+	}
+}
+
+
+
+
+
